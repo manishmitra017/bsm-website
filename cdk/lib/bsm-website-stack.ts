102492c1232022d603118cb7e4d7c3c61385f3f0
Manish Mitra <manishmitra017@gmail.com>
2025-09-03 23:38:27 +1000

    chore:domain name update

--- cdk/lib/bsm-website-stack.ts
+++ cdk/lib/bsm-website-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
@@ -12,6 +13,21 @@ import { Construct } from 'constructs';
 export class BsmWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const domainName = 'bsm.org.au';
+
+    // Create Route 53 hosted zone
+    const hostedZone = new route53.HostedZone(this, 'BsmHostedZone', {
+      zoneName: domainName,
+      comment: 'BSM Website hosted zone',
+    });
+
+    // Create SSL certificate for the domain
+    const certificate = new acm.Certificate(this, 'BsmCertificate', {
+      domainName: domainName,
+      subjectAlternativeNames: [`www.${domainName}`],
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
 
     // Create VPC
     const vpc = new ec2.Vpc(this, 'BsmVpc', {
@@ -57,6 +73,12 @@ export class BsmWebsiteStack extends cdk.Stack {
       cpu: 512,
       desiredCount: 2,
       publicLoadBalancer: true,
+      // HTTPS configuration
+      domainName: domainName,
+      domainZone: hostedZone,
+      certificate: certificate,
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      redirectHTTP: true,
       // Explicitly set platform architecture to x86_64
       platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
       runtimePlatform: {
@@ -99,22 +121,49 @@ export class BsmWebsiteStack extends cdk.Stack {
       targetUtilizationPercent: 80,
     });
 
-    // Output the load balancer URL
+    // Create alias record for www.bsm.org.au pointing to the load balancer
+    new route53.ARecord(this, 'BsmWWWAliasRecord', {
+      zone: hostedZone,
+      recordName: 'www',
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.LoadBalancerTarget(fargateService.loadBalancer)
+      ),
+    });
+
+    // Outputs
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: fargateService.loadBalancer.loadBalancerDnsName,
-      description: 'New Load Balancer DNS Name - Update bsm.org.au A record to point to this',
+      description: 'Load Balancer DNS Name',
     });
 
-    // Output the load balancer hosted zone ID for alias record
     new cdk.CfnOutput(this, 'LoadBalancerHostedZoneId', {
       value: fargateService.loadBalancer.loadBalancerCanonicalHostedZoneId,
-      description: 'Load Balancer Hosted Zone ID for Route53 alias record',
+      description: 'Load Balancer Hosted Zone ID',
     });
 
-    // Output the domain URL
     new cdk.CfnOutput(this, 'WebsiteURL', {
-      value: `https://bsm.org.au`,
-      description: 'BSM Website URL (after DNS update)',
+      value: `https://${domainName}`,
+      description: 'BSM Website URL with SSL certificate',
+    });
+
+    new cdk.CfnOutput(this, 'WWWWebsiteURL', {
+      value: `https://www.${domainName}`,
+      description: 'BSM Website WWW URL with SSL certificate',
+    });
+
+    new cdk.CfnOutput(this, 'CertificateArn', {
+      value: certificate.certificateArn,
+      description: 'SSL Certificate ARN',
+    });
+
+    new cdk.CfnOutput(this, 'HostedZoneId', {
+      value: hostedZone.hostedZoneId,
+      description: 'Route 53 Hosted Zone ID',
+    });
+
+    new cdk.CfnOutput(this, 'NameServers', {
+      value: cdk.Fn.join(', ', hostedZone.hostedZoneNameServers || []),
+      description: 'Route 53 Name Servers (update your domain registrar)',
     });
   }
 }
